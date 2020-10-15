30b310586cfb1f6cee44e83dcd260f715acd9d06
Jamie Stewart <stwjme@gmail.com>
2020-10-15 17:07:25 -0400

    Work-list

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-let items = ["Do Algorithms", "Continue Node.JS", "Complete JS"];
+let items = ["Algorithms", "Node.JS", "JavaScript"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
     
@@ -21,17 +23,32 @@ app.get("/", function(req, res){
 
     let day = today.toLocaleDateString("en-Us", options);
    
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
     });
 
     app.post("/", function (req, res){
+
         let item = req.body.newItem;
 
-        items.push(item);
-  
-        res.redirect("/");
+        if (req.body.list === "Work") {
+            workItems.push(item);
+            res.redirect("/work");
+        }else {
+            items.push(item);
+            res.redirect("/");
+
+        }
 });
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems:workItems});
+});
+
+app.post("/work", function (res, req) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
 
 
 
