f02ac318ac8c7e68e857aa54bf230aeecd72e28b
Jamie Stewart <stwjme@gmail.com>
2020-10-14 20:09:38 -0400

    Todo Updated

--- app.js
+++ app.js
@@ -3,42 +3,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Do Algorithms", "Continue Node.JS", "Complete JS"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-    }
-    res.render("list", {
-        kindOfDay: day
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-Us", options);
+   
+    res.render("list", {kindOfDay: day, newListItems: items});
     });
+
+    app.post("/", function (req, res){
+        let item = req.body.newItem;
+
+        items.push(item);
+  
+        res.redirect("/");
 });
 
 
